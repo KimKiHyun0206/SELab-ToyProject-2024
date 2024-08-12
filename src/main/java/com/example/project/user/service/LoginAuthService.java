602302884830f2f330ba23c911a9856dfbe056fe
Duskafka <rlarlgus0206@naver.com>
2024-08-12 18:17:52 +0900

    chore : rename

--- src/main/java/com/example/project/user/service/LoginAuthService.java
+++ src/main/java/com/example/project/user/service/LoginAuthService.java
@@ -14,7 +14,7 @@ import java.util.Base64;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class UserAuthService {
+public class LoginAuthService {
     private final UserService userService;
 
     /**
