421a681e52d1488a411beed63bb411dbc88cd21e
Duskafka <rlarlgus0206@naver.com>
2024-08-16 12:51:51 +0900

    fix : ROLE_TYPE 수정

--- src/main/java/com/example/project/user/domain/vo/RoleType.java
+++ src/main/java/com/example/project/user/domain/vo/RoleType.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum RoleType {
 
-    USER("ROLE_USER", "유저"),
-    ADMIN("ROLE_ADMIN", "관리자");
+    USER("USER", "유저"),
+    ADMIN("ADMIN", "관리자");
 
     private final String role;
     private final String value;
