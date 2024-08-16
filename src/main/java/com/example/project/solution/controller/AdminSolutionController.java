9a39f25edee9c34cf52c88562987b0c80c34a613
Duskafka <rlarlgus0206@naver.com>
2024-08-16 12:53:25 +0900

    delete : 불필요한 RestAPI 삭제

--- src/main/java/com/example/project/solution/controller/AdminSolutionController.java
+++ src/main/java/com/example/project/solution/controller/AdminSolutionController.java
@@ -5,10 +5,7 @@ import com.example.project.common.dto.ResponseMessage;
 import com.example.project.solution.dto.response.SolutionResponse;
 import com.example.project.solution.dto.request.admin.SolutionDeleteRequest;
 import com.example.project.solution.dto.request.admin.SolutionRegisterRequest;
-import com.example.project.solution.dto.request.admin.update.SolutionContextUpdateRequest;
-import com.example.project.solution.dto.request.admin.update.SolutionDifficultyUpdateRequest;
-import com.example.project.solution.dto.request.admin.update.SolutionExampleUpdateRequest;
-import com.example.project.solution.dto.request.admin.update.SolutionUpdateRequest;
+import com.example.project.solution.dto.request.admin.SolutionUpdateRequest;
 import com.example.project.solution.service.AdminSolutionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,39 +20,6 @@ import org.springframework.web.bind.annotation.*;
 public class AdminSolutionController {
 
     private final AdminSolutionService service;
-
-    /**
-     * @param request : Solution 의 Context 부분을 Update 할 수 있도록 정보를 가진 dto
-     * @return SolutionResponse : Update 된 Solution 에 대한 정보를 가지는 dto
-     * */
-    @PatchMapping("/context")
-    public ResponseEntity<?> contextUpdate(@RequestBody SolutionContextUpdateRequest request) {
-        var response = service.contextUpdate(request);
-
-        return ResponseDto.toResponseEntity(ResponseMessage.UPDATE_SUCCESS_SOLUTION, response);
-    }
-
-    /**
-     * @param request : Solution 의 Difficulty 부분을 Update 할 수 있도록 정보를 가진 dto
-     * @return SolutionResponse : Update 된 Solution 에 대한 정보를 가지는 dto
-     * */
-    @PatchMapping("/difficulty")
-    public ResponseEntity<?> difficultyUpdate(@RequestBody SolutionDifficultyUpdateRequest request) {
-        var response = service.difficultyUpdate(request);
-
-        return ResponseDto.toResponseEntity(ResponseMessage.UPDATE_SUCCESS_SOLUTION, response);
-    }
-
-    /**
-     * @param request : Solution 의 Example 부분을 Update 할 수 있도록 정보를 가진 dto
-     * @return SolutionResponse : Update 된 Solution 에 대한 정보를 가지는 dto
-     * */
-    @PatchMapping("/example")
-    public ResponseEntity<?> exampleUpdate(@RequestBody SolutionExampleUpdateRequest request) {
-        var response = service.exampleUpdate(request);
-
-        return ResponseDto.toResponseEntity(ResponseMessage.UPDATE_SUCCESS_SOLUTION, response);
-    }
 
     /**
      * @param request : Solution 을 Update 할 수 있도록 정보를 가진 dto
--- src/main/java/com/example/project/solution/service/AdminSolutionService.java
+++ src/main/java/com/example/project/solution/service/AdminSolutionService.java
@@ -1,34 +1,50 @@
 package com.example.project.solution.service;
 
+import com.example.project.error.dto.ErrorMessage;
+import com.example.project.error.exception.user.InvalidIdToFindUserException;
 import com.example.project.solution.dto.response.SolutionResponse;
 import com.example.project.solution.dto.request.admin.SolutionDeleteRequest;
 import com.example.project.solution.dto.request.admin.SolutionRegisterRequest;
-import com.example.project.solution.dto.request.admin.update.SolutionContextUpdateRequest;
-import com.example.project.solution.dto.request.admin.update.SolutionDifficultyUpdateRequest;
-import com.example.project.solution.dto.request.admin.update.SolutionExampleUpdateRequest;
-import com.example.project.solution.dto.request.admin.update.SolutionUpdateRequest;
+import com.example.project.solution.dto.request.admin.SolutionUpdateRequest;
 import com.example.project.solution.domain.Solution;
 import com.example.project.error.exception.solution.SolutionException;
 import com.example.project.solution.repository.SolutionRepository;
+import com.example.project.user.domain.User;
+import com.example.project.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
 public class AdminSolutionService {
     private final SolutionRepository solutionRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public SolutionResponse register(SolutionRegisterRequest request) {
-        Solution savedSolution = solutionRepository.save(request.toEntity());
+        User adminUser = userRepository.findById(request.getAdminId())
+                .orElseThrow(
+                        () -> new InvalidIdToFindUserException(ErrorMessage.USER_NOT_FOUND_ERROR, "유저를 찾을 수 없습니다")
+                );
 
-        return savedSolution.toResponseDto();
+        if (adminUser.getRoleType().getRole().equals("ADMIN")) {
+            Solution savedSolution = solutionRepository.save(request.toEntity());
+            return savedSolution.toResponseDto();
+        }
+
+        return null;
     }
 
     @Transactional
     public SolutionResponse delete(SolutionDeleteRequest request) {
-        if (request.getAdminId().equals("admin")) {
+        User adminUser = userRepository.findById(request.getAdminId())
+                .orElseThrow(
+                        () -> new InvalidIdToFindUserException(ErrorMessage.USER_NOT_FOUND_ERROR, "유저를 찾을 수 없습니다")
+                );
+
+        if (adminUser.getRoleType().getRole().equals("ADMIN")) {
             if (solutionRepository.existsById(request.getSolutionId())) {
                 Solution solution = solutionRepository.findById(request.getSolutionId()).orElseThrow(SolutionException::new);
                 solutionRepository.deleteById(request.getSolutionId());
@@ -38,53 +54,15 @@ public class AdminSolutionService {
         return null;
     }
 
-    private boolean isAdmin(String id) {
-        return id.equals("admin");
-    }
-
-    @Transactional
-    public SolutionResponse contextUpdate(SolutionContextUpdateRequest request) {
-        if (isAdmin(request.getAdminId())) {
-            Solution solution = solutionRepository.findById(request.getSolutionId())
-                    .orElseThrow(SolutionException::new);
-
-            solution.updateContext(request.getTitle(), request.getDescription());
-
-            return solution.toResponseDto();
-        }
-
-        return null;
-    }
-
-    @Transactional
-    public SolutionResponse difficultyUpdate(SolutionDifficultyUpdateRequest request) {
-        if (isAdmin(request.getAdminId())) {
-            Solution solution = solutionRepository.findById(request.getSolutionId())
-                    .orElseThrow(SolutionException::new);
-
-            solution.updateDifficulty(request.getDifficulty());
-
-            return solution.toResponseDto();
-        }
-        return null;
-    }
-
-    @Transactional
-    public SolutionResponse exampleUpdate(SolutionExampleUpdateRequest request) {
-        if (isAdmin(request.getAdminId())) {
-            Solution solution = solutionRepository.findById(request.getSolutionId())
-                    .orElseThrow(SolutionException::new);
-
-            solution.updateExample(request.getInExample(), request.getOutExample());
-
-            return solution.toResponseDto();
-        }
-        return null;
-    }
-
     @Transactional
     public SolutionResponse updateAll(SolutionUpdateRequest request) {
-        if (isAdmin(request.getAdminId())) {
+
+        User adminUser = userRepository.findById(request.getAdminId())
+                .orElseThrow(
+                        () -> new InvalidIdToFindUserException(ErrorMessage.USER_NOT_FOUND_ERROR, "유저를 찾을 수 없습니다")
+                );
+
+        if (adminUser.getRoleType().getRole().equals("ADMIN")) {
             Solution solution = solutionRepository.findById(request.getSolutionId())
                     .orElseThrow(SolutionException::new);
 
