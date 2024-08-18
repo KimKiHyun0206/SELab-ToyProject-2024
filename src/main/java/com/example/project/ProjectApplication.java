abda948d873013c2551fcb1c6fc8e5bbccc36081
Duskafka <rlarlgus0206@naver.com>
2024-08-18 12:54:00 +0900

    update(Application) : Application init 추가
    타임존 설정으로 서버 시간이 서울로 고정되도록 함.

--- src/main/java/com/example/project/ProjectApplication.java
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -1,15 +1,43 @@
 package com.example.project;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.Arrays;
+import java.util.TimeZone;
+
+@Slf4j
 @EnableAsync
 @SpringBootApplication
-public class ProjectApplication {
+@RequiredArgsConstructor
+public class ProjectApplication implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final Environment environment;
 
     public static void main(String[] args) {
+        init();
         SpringApplication.run(ProjectApplication.class, args);
     }
 
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        log.info("applicationReady status{}", Arrays.toString(environment.getActiveProfiles()));
+    }
+
+    @Override
+    public boolean supportsAsyncExecution() {
+        return ApplicationListener.super.supportsAsyncExecution();
+    }
+
+    private static void init() {
+        log.info("Spring Server TimeZone : Asia/Seoul");
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
