9fd07dc244b72e1e5823f19c0ea7acac264d60e5
Duskafka <rlarlgus0206@naver.com>
2024-09-09 17:41:34 +0900

    update(JWT): JWT 인증을 해주는 로그인 메소드 생성

--- src/main/java/com/example/project/user/controller/UserLoginController.java
+++ src/main/java/com/example/project/user/controller/UserLoginController.java
@@ -2,6 +2,8 @@ package com.example.project.user.controller;
 
 import com.example.project.error.exception.user.InvalidLoginUserIdException;
 import com.example.project.error.exception.user.InvalidLoginPasswordException;
+import com.example.project.jwt.token.TokenProvider;
+import com.example.project.jwt.token.TokenResolver;
 import com.example.project.user.dto.UserResponse;
 import com.example.project.user.dto.login.LoginRequest;
 import com.example.project.user.service.CookieService;
@@ -12,6 +14,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,14 +30,34 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class UserLoginController {
     private final LoginService loginService;
-    private final UserService userService;
     private final SessionService sessionService;
     private final CookieService cookieService;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
+    private final TokenProvider tokenProvider;
+
+
+    public void jwtAuthLogin(
+            @RequestBody LoginRequest loginRequest,
+            HttpServletResponse httpServletResponse
+    ) throws IOException {
+        log.info("jwtAuthLogin {}, {}", loginRequest.getUserId(), loginRequest.getPassword());
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginRequest.getUserId(), loginRequest.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.createToken(authentication);
+        log.info("authrize jwt {}", jwt);
+
+        httpServletResponse.addHeader(TokenResolver.AUTHORIZATION_HEADER, "Bearer " + jwt);
+        httpServletResponse.sendRedirect("http://localhost:8080/");
+    }
 
     @PostMapping("/login")
     public void login(
-            LoginRequest loginRequest,
+            @RequestBody LoginRequest loginRequest,
             Model model,
             @CookieValue(value = "DigitalLoginCookie", required = false) String cookieValue,
             HttpServletRequest request,
