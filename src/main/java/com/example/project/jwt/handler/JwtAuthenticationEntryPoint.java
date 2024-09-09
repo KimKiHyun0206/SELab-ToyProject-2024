98affe2fd0739fa182327dadade5caa6429948aa
Duskafka <rlarlgus0206@naver.com>
2024-09-09 17:42:44 +0900

    chore(Log): 로깅
    각각 핸들러에 어떤 핸들러에 요청이 들어갔는지 알 수 있도록 로깅을 추가함

--- src/main/java/com/example/project/jwt/handler/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/project/jwt/handler/JwtAuthenticationEntryPoint.java
@@ -2,12 +2,14 @@ package com.example.project.jwt.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
    @Override
@@ -15,6 +17,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
                         HttpServletResponse response,
                         AuthenticationException authException) throws IOException {
       // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
+      log.info("유효한 자격증명이 없는 접근입니다");
       response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
    }
 }
