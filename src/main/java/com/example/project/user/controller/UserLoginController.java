c97ad39b02c08fcef3b056d02e2dde084b5b1c3f
Duskafka <rlarlgus0206@naver.com>
2024-09-09 20:26:16 +0900

    update(JWT): JWT Login
    로그인을 하면 JWT 토큰을 발행해주는 login 메소드 추가
    
    만약 로그인이 실패할 시 401 코드를 보내며 이를 JS에서 처리할 수 있도록 할 것

--- src/main/java/com/example/project/user/controller/UserLoginController.java
+++ src/main/java/com/example/project/user/controller/UserLoginController.java
@@ -15,6 +15,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -29,79 +30,29 @@ import java.io.IOException;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserLoginController {
-    private final LoginService loginService;
-    private final SessionService sessionService;
-    private final CookieService cookieService;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
-
     private final TokenProvider tokenProvider;
 
-
-    public void jwtAuthLogin(
-            @RequestBody LoginRequest loginRequest,
-            HttpServletResponse httpServletResponse
-    ) throws IOException {
-        log.info("jwtAuthLogin {}, {}", loginRequest.getUserId(), loginRequest.getPassword());
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(loginRequest.getUserId(), loginRequest.getPassword());
-
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String jwt = tokenProvider.createToken(authentication);
-        log.info("authrize jwt {}", jwt);
-
-        httpServletResponse.addHeader(TokenResolver.AUTHORIZATION_HEADER, "Bearer " + jwt);
-        httpServletResponse.sendRedirect("http://localhost:8080/");
-    }
 
     @PostMapping("/login")
     public void login(
             @RequestBody LoginRequest loginRequest,
-            Model model,
-            @CookieValue(value = "DigitalLoginCookie", required = false) String cookieValue,
-            HttpServletRequest request,
-            HttpServletResponse response
-    ) throws IOException {
-        log.info("Login Try -> id : {}, password : {}", loginRequest.getUserId(), loginRequest.getPassword());
+            HttpServletResponse httpServletResponse
+    ) {
+        log.info("jwtAuthLogin {}, {}", loginRequest.getUserId(), loginRequest.getPassword());
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginRequest.getUserId(), loginRequest.getPassword());
 
-        try {
-            UserResponse userResponse;
 
-            if (cookieValue == null) userResponse = noCookieLogin(loginRequest);
-            else userResponse = cookieLogin(loginRequest, cookieValue, request);
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-            sessionService.sessionRegistration(request, userResponse);
-            response.addCookie(cookieService.authCookieIssue(loginRequest));
-            //TODO loginInfo cookie 반환하도록 수정
-            response.addCookie(cookieService.loginInfoCookieIssue(loginRequest));
-            response.sendRedirect("http://localhost:8080/");
+        if(authentication.isAuthenticated()){
+            String jwt = tokenProvider.createToken(authentication);
+            log.info("authrize jwt {}", jwt);
 
-        } catch (InvalidLoginUserIdException e) {
-            log.info(e.getMessage());
-            model.addAttribute("IdError", "입력한 ID " + loginRequest.getUserId() + "가 존재하지 않습니다");
-            response.sendRedirect("http://localhost:8080/user/login");
-
-        } catch (InvalidLoginPasswordException e) {
-            log.info(e.getMessage());
-            model.addAttribute("PasswordError", "입력한 PASSWORD " + loginRequest.getPassword() + "가 존재하지 않습니다");
-            response.sendRedirect("http://localhost:8080/user/login");
+            httpServletResponse.setHeader(TokenResolver.AUTHORIZATION_HEADER, "Bearer " + jwt);
+            httpServletResponse.setStatus(HttpStatus.OK.value());
         }
-    }
-
-    private UserResponse noCookieLogin(
-            LoginRequest loginRequest
-    ) throws InvalidLoginUserIdException, InvalidLoginPasswordException {
-        return loginService.login(loginRequest.getUserId(), loginRequest.getPassword());
-    }
-
-    private UserResponse cookieLogin(LoginRequest loginRequest, String cookieValue, HttpServletRequest request) throws InvalidLoginUserIdException, InvalidLoginPasswordException {
-        var userResponse = sessionService.getUser(request, cookieValue);
-
-        if (userResponse == null) {
-            return loginService.login(loginRequest.getUserId(), loginRequest.getPassword());
-        }
-
-        return userResponse;
     }
 }
