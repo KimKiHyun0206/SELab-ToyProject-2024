903e6521773f765e71a2a082829ffa9395f1a357
Duskafka <rlarlgus0206@naver.com>
2024-09-10 18:27:27 +0900

    chore(Auth): 파일명 변경

--- src/main/java/com/example/project/jwt/repository/JWTTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.project.jwt.repository;
-
-import com.example.project.jwt.domain.JWTToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JWTTokenRepository extends JpaRepository<JWTToken, Long> {
-}
