46e433534f89091066c117c4a1fa7d7f866b4ebe
Duskafka <rlarlgus0206@naver.com>
2024-09-10 18:29:09 +0900

    update(Auth): 발행된 토큰을 저장할 수 있도록 함
    기존에 토큰을 발행해도 서버 측에서 이를 저장하지 않아서 정상적인 토큰인지 알 수 없었음.
    하지만 이를 데이터베이스에 저장하여 서버가 발급해준 것인지 그리고 그것이 정상적인 토큰인지 알 수 있도록 함.

---
+++ src/main/java/com/example/project/auth/dto/AuthTokenResponse.java
@@ -0,0 +1,14 @@
+package com.example.project.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthTokenResponse {
+    private Long id;
+
+    private Long userId;
+
+    private String tokenValue;
+}
---
+++ src/main/java/com/example/project/auth/service/AuthTokenService.java
@@ -0,0 +1,41 @@
+package com.example.project.auth.service;
+
+import com.example.project.auth.domain.AuthToken;
+import com.example.project.auth.dto.AuthTokenResponse;
+import com.example.project.auth.repository.AuthTokenRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthTokenService {
+    private final AuthTokenRepository authTokenRepository;
+
+    @Transactional
+    public AuthTokenResponse registerToken(Long userId, String token){
+        AuthToken saved = authTokenRepository.save(new AuthToken(userId,token));
+        log.info("AuthToken Saved id:{}, userId:{}, token:{}",saved.getId(),saved.getUserId(),saved.getToken());
+
+        return saved.toResponseDto();
+    }
+
+    @Transactional(readOnly = true)
+    public boolean isValidateToken(String token){
+        AuthToken foundAuthToken = authTokenRepository.findAuthTokenByToken(token);
+
+        return foundAuthToken != null;
+    }
+
+    @Transactional
+    public void deleteToken(String token){
+        log.info("deleteToken -> {}", token);
+        if (authTokenRepository.existsByToken(token)){
+            log.info("token exists in database -> {}",token);
+            authTokenRepository.deleteAuthTokenByToken(token);
+            log.info("token deleted -> {}", token);
+        }
+    }
+}
