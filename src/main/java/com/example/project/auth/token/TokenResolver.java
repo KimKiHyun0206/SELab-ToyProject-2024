0ab0a4b3b6cb57053f520250af71d8818d485410
Duskafka <rlarlgus0206@naver.com>
2024-09-12 23:05:10 +0900

    update(JWT): Token 생성 방식 변경

--- src/main/java/com/example/project/auth/token/TokenResolver.java
+++ src/main/java/com/example/project/auth/token/TokenResolver.java
@@ -1,23 +1,22 @@
 package com.example.project.auth.token;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+@Slf4j
 @Component
 public class TokenResolver {
-    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String AUTHORIZATION_HEADER = "code-for-code-auth";
     /**
      * @implSpec 이 메소드에서는 request header에 포함되어있는 JWT 토큰 값을 가져오게 해준다
      * */
     public String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        String token = request.getHeader(AUTHORIZATION_HEADER);
 
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
+        log.info("Resolve Token -> {}", token);
 
-        return null;
+        return token;
     }
-
 }
