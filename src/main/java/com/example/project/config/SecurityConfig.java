1f68d20e554692d2b1508d3a19ce063bb1493e27
Duskafka <rlarlgus0206@naver.com>
2024-09-12 19:33:14 +0900

    update(JS): 토큰 로그인 성공

--- src/main/java/com/example/project/config/SecurityConfig.java
+++ src/main/java/com/example/project/config/SecurityConfig.java
@@ -28,47 +28,43 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
-                .csrf(AbstractHttpConfigurer::disable)
+            // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+            .csrf(AbstractHttpConfigurer::disable)
 
-                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(exceptionHandling -> exceptionHandling
-                        .accessDeniedHandler(jwtAccessDeniedHandler)
-                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                )
+            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling(exceptionHandling -> exceptionHandling
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+            )
 
-                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-                        .requestMatchers(
-                                "/api/hello",
-                                "/api/authenticate",
-                                "/api/signup",
-                                "/",
-                                "/solutions",
-                                "/solutions/list",
-                                "/users/login",
-                                "/users/register",
-                                "/js/**",
-                                "/css/**",
-                                "/api/users/login",
-                                "/api/authenticate",
-                                "/compile",
-                                "/api/compile"
-                        ).permitAll()
-                        //.requestMatchers(PathRequest.toH2Console()).permitAll()
-                        .anyRequest().authenticated()
-                )
+            .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                .requestMatchers(
+                        "/api/hello",
+                        "/",
+                        "/solutions",
+                        "/solutions/list",
+                        "/users/login",
+                        "/users/register",
+                        "/js/**",
+                        "/css/**",
+                        "/api/**"
 
-                // 세션을 사용하지 않기 때문에 STATELESS로 설정
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                )
+                ).permitAll()
+                //.requestMatchers(PathRequest.toH2Console()).permitAll()
+                .anyRequest().authenticated()
+            )
 
-                // enable h2-console
-                .headers(headers ->
-                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
-                )
+            // 세션을 사용하지 않기 때문에 STATELESS로 설정
+            .sessionManagement(sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+            )
 
-                .with(jwtSecurityConfig, customizer -> {});
+            // enable h2-console
+            .headers(headers ->
+                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+            )
+
+            .with(jwtSecurityConfig, customizer -> {});
         return http.build();
     }
 }
