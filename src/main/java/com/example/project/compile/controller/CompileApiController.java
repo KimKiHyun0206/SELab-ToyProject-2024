37d9f75202b387acf4439dea3ca0c48c0b8796a8
kangsangmoon <42689622+kangsangmoon@users.noreply.github.com>
2024-09-12 15:24:56 +0900

    update(CompileApiController) : jwt 인증 추가 및 DTO 변경
    - JWT 토큰 유효성 검사 추가
    - Authorization 헤더로 인증 처리
    - CompileRequest로 요청 DTO 변경

--- src/main/java/com/example/project/compile/controller/CompileApiController.java
+++ src/main/java/com/example/project/compile/controller/CompileApiController.java
@@ -1,26 +1,35 @@
 package com.example.project.compile.controller;
 
+import com.example.project.auth.service.AuthTokenService;
 import com.example.project.common.dto.ResponseDto;
 import com.example.project.common.dto.ResponseMessage;
-import com.example.project.solution.dto.request.user.SolutionCompileRequest;
+import com.example.project.compile.dto.CompileRequest;
 import com.example.project.compile.service.CompileService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/compile")
 public class CompileApiController {
 
     private final CompileService compileService;
+    private final AuthTokenService authTokenService;
 
     @PostMapping
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<?> compileCode(@RequestBody SolutionCompileRequest request) {
+    public ResponseEntity<?> compileCode(@RequestHeader("Authorization") String token, @RequestBody CompileRequest request) {
+        String jwt = token.replace("Bearer ", "");
+
+        if (!authTokenService.isValidateToken(jwt)) {
+            log.info("Token: {}", jwt);
+            return ResponseDto.toResponseEntity(ResponseMessage.INVALID_TOKEN_EXCEPTION, jwt);
+        }
+
         try {
             String result = compileService.compileAndRun(request.getLanguage(), request.getCode());
             return ResponseDto.toResponseEntity(ResponseMessage.COMPILE_SUCCESS, result);
