fe370d34a45397e3fb68b26024b2d40522c2caa1
kangsangmoon <42689622+kangsangmoon@users.noreply.github.com>
2024-09-16 21:03:49 +0900

    refactor(controller) : 분할 된 서비스에 따라 컨트롤러 수정

--- src/main/java/com/example/project/compile/controller/rest/CompileApiController.java
+++ src/main/java/com/example/project/compile/controller/rest/CompileApiController.java
@@ -5,10 +5,11 @@ import com.example.project.common.dto.ResponseDto;
 import com.example.project.common.dto.ResponseMessage;
 import com.example.project.compile.dto.CompileRequest;
 import com.example.project.compile.service.CompileService;
+import com.example.project.error.dto.ErrorMessage;
+import com.example.project.error.dto.ErrorResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -18,30 +19,31 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @RequestMapping("/api/compile")
 public class CompileApiController {
+
     private final CompileService compileService;
     private final AuthTokenService authTokenService;
 
     @PostMapping
-    public ResponseEntity<?> compileCode(@RequestHeader("Authorization") String token,
+    public ResponseEntity<?> compileCode(@RequestHeader("code-for-code-auth") String token,
                                          @RequestBody CompileRequest request) {
-        String jwt = token.replace("Bearer ", "");
+        String jwt = token;
 
-        if (!authTokenService.isValidateToken(jwt)) {
+        if (authTokenService.isValidateToken(jwt)) {
             log.info("Token: {}", jwt);
-            return ResponseDto.toResponseEntity(ResponseMessage.INVALID_TOKEN_EXCEPTION, jwt);
-        }
+            try {
+                String result = compileService.compileAndRun(request.getLanguage(), request.getCode());
+                return ResponseDto.toResponseEntity(ResponseMessage.COMPILE_SUCCESS, result);
+            } catch (IllegalArgumentException e) {
+                return ResponseDto.toResponseEntity(ResponseMessage.INVALID_LANGUAGE, e.getMessage());
+            } catch (IOException e) {
+                return ResponseDto.toResponseEntity(ResponseMessage.IO_ERROR, e.getMessage());
+            } catch (InterruptedException e) {
+                return ResponseDto.toResponseEntity(ResponseMessage.EXECUTION_INTERRUPTED, e.getMessage());
+            } catch (Exception e) {
+                return ResponseDto.toResponseEntity(ResponseMessage.GENERAL_COMPILE_ERROR, e.getMessage());
+            }
+        } else return ErrorResponseDto.of(ErrorMessage.NOT_FOUND_CLIENT_ID_HEADER);
 
-        try {
-            String result = compileService.compileAndRun(request.getLanguage(), request.getCode());
-            return ResponseDto.toResponseEntity(ResponseMessage.COMPILE_SUCCESS, result);
-        } catch (IllegalArgumentException e) {
-            return ResponseDto.toResponseEntity(ResponseMessage.INVALID_LANGUAGE, e.getMessage());
-        } catch (IOException e) {
-            return ResponseDto.toResponseEntity(ResponseMessage.IO_ERROR, e.getMessage());
-        } catch (InterruptedException e) {
-            return ResponseDto.toResponseEntity(ResponseMessage.EXECUTION_INTERRUPTED, e.getMessage());
-        } catch (Exception e) {
-            return ResponseDto.toResponseEntity(ResponseMessage.GENERAL_COMPILE_ERROR, e.getMessage());
-        }
     }
 }
+
