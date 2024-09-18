eee3ac368f26adb6b888029a132252cdb4fd194d
kangsangmoon <42689622+kangsangmoon@users.noreply.github.com>
2024-09-18 22:00:51 +0900

    refactor(CompileController) : HttpServletRequest로 토큰 가져오기
    - HttpServletRequest로 토큰값을 가져와서 인증이 되면 컴파일하게 수정

--- src/main/java/com/example/project/compile/controller/rest/CompileApiController.java
+++ src/main/java/com/example/project/compile/controller/rest/CompileApiController.java
@@ -3,10 +3,12 @@ package com.example.project.compile.controller.rest;
 import com.example.project.auth.service.AuthTokenService;
 import com.example.project.common.dto.ResponseDto;
 import com.example.project.common.dto.ResponseMessage;
+import com.example.project.common.util.HeaderUtil;
 import com.example.project.compile.dto.CompileRequest;
 import com.example.project.compile.service.CompileService;
 import com.example.project.error.dto.ErrorMessage;
 import com.example.project.error.dto.ErrorResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -24,12 +26,9 @@ public class CompileApiController {
     private final AuthTokenService authTokenService;
 
     @PostMapping
-    public ResponseEntity<?> compileCode(@RequestHeader("code-for-code-auth") String token,
-                                         @RequestBody CompileRequest request) {
-        String jwt = token;
+    public ResponseEntity<?> compileCode(@RequestBody CompileRequest request, HttpServletRequest httpServletRequest) throws IOException {
 
-        if (authTokenService.isValidateToken(jwt)) {
-            log.info("Token: {}", jwt);
+        if (authTokenService.isValidateToken(HeaderUtil.resolveToken(httpServletRequest))) {
             try {
                 String result = compileService.compileAndRun(request.getLanguage(), request.getCode());
                 return ResponseDto.toResponseEntity(ResponseMessage.COMPILE_SUCCESS, result);
