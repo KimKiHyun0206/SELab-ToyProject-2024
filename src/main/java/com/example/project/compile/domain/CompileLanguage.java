006abb49e44b62c3cc4845ad283928701134b40c
kangsangmoon <42689622+kangsangmoon@users.noreply.github.com>
2024-09-18 21:59:31 +0900

    refactor(CompileLanguage) : 메서드명 및 클래스명 변경
    - 메서드명 fromString에서 getByLanguageName으로 변경
    - 클래스명 Language에서 CompileLanguage로 수정

--- src/main/java/com/example/project/compile/domain/CompileLanguage.java
+++ src/main/java/com/example/project/compile/domain/CompileLanguage.java
@@ -1,8 +1,10 @@
 package com.example.project.compile.domain;
 
 import com.example.project.error.dto.ErrorMessage;
+import lombok.Getter;
 
-public enum Language {
+@Getter
+public enum CompileLanguage {
     C(".c", "gcc %s -o %s"),
     CPP(".cpp", "g++ %s -o %s"),
     JAVA(".java", "javac -encoding UTF-8 %s"),
@@ -12,7 +14,7 @@ public enum Language {
     private final String extension;
     private final String compileCommand;
 
-    Language(String extension, String compileCommand) {
+    CompileLanguage(String extension, String compileCommand) {
         this.extension = extension;
         this.compileCommand = compileCommand;
     }
@@ -25,7 +27,7 @@ public enum Language {
         return compileCommand;
     }
 
-    public static Language fromString(String language) {
+    public static CompileLanguage getByLanguageName(String language) {
         return switch (language.toLowerCase()) {
             case "c" -> C;
             case "cpp", "c++" -> CPP;
