9e2dabcab9391b30d5d8cf5fce9679ee743cf2cc
kangsangmoon <42689622+kangsangmoon@users.noreply.github.com>
2024-09-18 22:03:50 +0900

    refactor(FileService) : FileService 개선 및 코드 디렉터리 설정 변경
    - @Value 어노테이션을 사용하여 외부 설정으로 주입
    - Java 파일 생성 시 클래스 이름을 기반으로 파일 이름을 생성
    - Java 코드로부터 클래스 이름을 추출하는 extractClassName 메서드 추가
    - Files.writeString()를 사용하여 간결화

--- src/main/java/com/example/project/compile/service/FileService.java
+++ src/main/java/com/example/project/compile/service/FileService.java
@@ -1,6 +1,7 @@
 package com.example.project.compile.service;
 
-import com.example.project.compile.domain.Language;
+import com.example.project.compile.domain.CompileLanguage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -12,25 +13,43 @@ import java.util.UUID;
 @Service
 public class FileService {
 
-    private static final String CODE_DIR = "/home/ubuntu/code/";
+    private final String code_dir;
 
-    public Path createCodeFile(String code, Language language) throws IOException {
-        Path codePath = Paths.get(CODE_DIR);
+    public FileService(@Value("${compile.url}") String codeDir) {
+        code_dir = codeDir;
+    }
+
+    public Path createCodeFile(String code, CompileLanguage compileLanguage) throws IOException {
+        Path codePath = Paths.get(code_dir);
         if (!Files.exists(codePath)) {
             Files.createDirectories(codePath);
         }
 
-        String filename = generateFileName(language);
+        String filename;
+        if (compileLanguage == CompileLanguage.JAVA) {
+            filename = extractClassName(code) + compileLanguage.getExtension();
+        } else {
+            filename = generateFileName(compileLanguage);
+        }
         Path filePath = codePath.resolve(filename);
-        Files.write(filePath, code.getBytes());
+        Files.writeString(filePath, code);
         return filePath;
+    }
+
+    public String extractClassNameFromFile(Path filePath) throws IOException {
+        String code = Files.readString(filePath);
+        return extractClassName(code);
+    }
+
+    private String extractClassName(String code) {
+        return code.split("public class ")[1].split("\\s")[0].trim();
     }
 
     public void deleteCodeFile(Path filePath) throws IOException {
         Files.deleteIfExists(filePath);
     }
 
-    private String generateFileName(Language language) {
+    private String generateFileName(CompileLanguage language) {
         return UUID.randomUUID() + language.getExtension();
     }
 }
