d5ad0f6456509da39589c763711f9dde71a3b4f9
kangsangmoon <42689622+kangsangmoon@users.noreply.github.com>
2024-09-21 00:03:39 +0900

    refactor(Compile) : 코드 디렉토리 매개변수화 및 예외 처리
    - createCodeFile 메서드에서 codeDir을 매개변수로 받아 필드 제거
    - 파일 경로 생성 시 InvalidPathException 및 SecurityException 처리
    - UUID를 사용하여 고유한 파일 이름 생성으로 파일 충돌 방지

--- src/main/java/com/example/project/compile/service/FileService.java
+++ src/main/java/com/example/project/compile/service/FileService.java
@@ -1,11 +1,12 @@
 package com.example.project.compile.service;
 
 import com.example.project.compile.domain.CompileLanguage;
-import org.springframework.beans.factory.annotation.Value;
+import com.example.project.error.dto.ErrorMessage;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
@@ -13,46 +14,24 @@ import java.util.UUID;
 @Service
 public class FileService {
 
-    private final String codeDir;   //한 번 쓰는 변수인데 이걸 필드값으로 가질 이유가 있나요?
+    public Path createCodeFile(String code, CompileLanguage compileLanguage, String codeDir) throws IOException {
+        try {
+            Path codePath = Paths.get(codeDir);
+            if (!Files.exists(codePath)) {
+                Files.createDirectories(codePath);
+            }
 
-    public FileService(@Value("${compile.url}") String codeDir) {
-        this.codeDir = codeDir;
-    }
+            //UUID를 사용하는 이유는  고유한 파일 이름을 생성하여 파일의 충돌을 방지하기 위함입니다.
+            String filename = "Main_" + UUID.randomUUID() + compileLanguage.getExtension();
 
-    public Path createCodeFile(String code, CompileLanguage compileLanguage) throws IOException {
-        Path codePath = Paths.get(codeDir);
-        if (!Files.exists(codePath)) {
-            Files.createDirectories(codePath);
+            Path filePath = codePath.resolve(filename);
+            Files.writeString(filePath, code);
+            return filePath;
+
+        } catch (InvalidPathException e) {
+            throw new IOException(ErrorMessage.INVALID_PATH_EXCEPTION.getMessage(), e);
+        } catch (SecurityException e) {
+            throw new IOException(ErrorMessage.ACCESS_PERMISSION_EXCEPTION.getMessage(), e);
         }
-
-        String filename;
-        if (compileLanguage == CompileLanguage.JAVA) {
-            filename = extractClassName(code) + compileLanguage.getExtension();
-        } else {
-            filename = generateFileName(compileLanguage);
-        }
-        Path filePath = codePath.resolve(filename);
-        Files.writeString(filePath, code);
-        return filePath;
-    }
-
-    public String extractClassNameFromFile(Path filePath) throws IOException {
-        String code = Files.readString(filePath);
-        return extractClassName(code);
-    }
-
-    //원래는 하나였던 메소드를 위의 메소드에서 더 나눈 이유가 뭔가요?
-    private String extractClassName(String code) {
-        return code.split("public class ")[1].split("\\s")[0].trim();
-    }
-
-    //이 한 줄만 처리한다면 메소드로 나눌 이유가 있을까요?
-    public void deleteCodeFile(Path filePath) throws IOException {
-        Files.deleteIfExists(filePath);
-    }
-
-    private String generateFileName(CompileLanguage language) {
-        //UUID를 받아오는 이유가 뭔가요
-        return UUID.randomUUID() + language.getExtension();
     }
 }
