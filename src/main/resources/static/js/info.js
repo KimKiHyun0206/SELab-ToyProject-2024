ec4afcc9a988fa8200cc2dff66d0f553e705efdb
Minseob So <pksomin@gmail.com>
2024-09-26 17:07:40 +0900

    fix: info js 수정

--- src/main/resources/static/js/info.js
+++ src/main/resources/static/js/info.js
@@ -1,23 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('editForm');
-    form.addEventListener('submit', async function(event) {
-        event.preventDefault(); // 기본 폼 제출 방지
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('userId')
+    const hidden = document.getElementById("hidden").value
+    button.addEventListener('click', async () => {
 
-        const userId = document.getElementById('userId').value;
-        const token = localStorage.getItem('code-for-code-auth'); // 토큰 가져오기
-
-        if (userId) {
+        const headers = { 'code-for-code-auth': localStorage.getItem('code-for-code-auth') } // 토큰 가져오기
+        if (headers) {
             try {
-                const response = await fetch(`/edit-info/${userId}`, {
-                    method: 'GET',
-                    headers: {
-                        'code-for-code-auth': `${token}`
-                    }
-                });
-
+                const response = await(await fetch(`/api/users/info/edit/${hidden}`, {
+                    headers
+                })).json()
+                console.log(hidden,headers,response)
                 if (response.ok) {
                     // 성공적으로 요청이 완료된 경우
-                    window.location.href = `/edit-info/'${userId}`; // 해당 페이지로 이동
+                    window.location.href = `/api/users/info/edit/${hidden}`; // 해당 페이지로 이동
                 } else {
                     alert('페이지를 로드하는데 실패했습니다.');
                 }
@@ -26,10 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert('서버 오류가 발생했습니다.');
             }
         } else {
-            alert('UserID가 없습니다.');
+            alert('로그인을 해주세요.');
         }
     });
-});
-
-
-
+})
